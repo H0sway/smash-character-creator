3fcd75924f4754ec75f6284e35940c364574b187
Joshua Russell <jkrussell756@gmail.com>
2018-08-07 12:24:36 -0400

    Created serializers for each model so the data is sent out as JSON objects

---
+++ character_creator/characters/serializers.py
@@ -0,0 +1,108 @@
+# Import modules, models
+from rest_framework import serializers
+from characters.models import .
+
+class CharacterSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Character
+        fields = '__all__'
+
+class NeutralAttackSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Neutral_Attack
+        fields = '__all__'
+
+class ForwardTiltSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Forward_Tilt
+        fields = '__all__'
+
+class UpTiltSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Up_Tilt
+        fields = '__all__'
+
+class DownTiltSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Down_Tilt
+        fields = '__all__'
+
+class ForwardSmashSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Forward_Smash
+        fields = '__all__'
+
+class UpSmashSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Up_Smash
+        fields = '__all__'
+
+class DownSmashSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Down_Smash
+        fields = '__all__'
+
+class DashAttackSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Dash_Attack
+        fields = '__all__'
+
+class ForwardArialSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Forward_Arial
+        fields = '__all__'
+
+class BackwardArialSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Backward_Arial
+        fields = '__all__'
+
+class UpArialSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Up_Arial
+        fields = '__all__'
+
+class DownArialSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Down_Arial
+        fields = '__all__'
+
+class ForwardThrowSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Forward_Throw
+        fields = '__all__'
+
+class BackwardThrowSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Backward_Throw
+        fields = '__all__'
+
+class UpThrowSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Up_Throw
+        fields = '__all__'
+
+class DownThrowSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Down_Throw
+        fields = '__all__'
+
+class SideSpecialSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Side_Special
+        fields = '__all__'
+
+class UpSpecialSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Up_Special
+        fields = '__all__'
+
+class DownSpecialSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Down_Special
+        fields = '__all__'
+
+class FinalSmashSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Final_Smash
+        fields = '__all__'
