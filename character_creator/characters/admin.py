f7602b038882688bac5e402f481d9803bac6c37f
Joshua Russell <jkrussell756@gmail.com>
2018-08-07 18:16:03 -0400

    configured the admin page

--- character_creator/characters/admin.py
+++ character_creator/characters/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Character, Moveset
+
+class MovesetInline(admin.TabularInline):
+    model = Moveset
+    extra = 23
+
+class CharacterAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name',                  {'fields': ['name']}),
+        ('Date Published',        {'fields': ['pub_date']}),
+    ]
+    inlines = [MovesetInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Character, CharacterAdmin)
