9d2f5e10a5b14c178c38bd2d3762f93d03b54120
Joshua Russell <jkrussell756@gmail.com>
2018-09-06 16:20:05 -0400

    css not properly getting added to the build. HTML elements seem to be fine but classes aren't working

--- character_creator/frontend/src/components/Home.js
+++ character_creator/frontend/src/components/Home.js
@@ -1,18 +1,21 @@
 // Import dependencies, bootstrap elements
 import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+
+import './styles/Home.css';
 
 // Functional component, no need to use a React component for now.
 const Home = () => {
   return (
     <div className="Home">
-      <div className="home-title">
+      <Jumbotron className="home-title">
         <h1>WELCOME</h1>
         <h3>Conceptualize the characters you want to see in Smash!</h3>
-      </div>
-      <LinkContainer to="/create">
-        <button className="home-button">Create a Character</button>
-      </LinkContainer>
+        <LinkContainer to="/create">
+          <button className="home-button">Create a Character</button>
+        </LinkContainer>
+      </Jumbotron>
     </div>
   )
 }
